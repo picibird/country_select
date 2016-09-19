20fe409a4975031ab374aa2c744b47455b0ea419
Denny Mueller <denny@dennymueller.de>
2016-09-19 11:43:25 +0200

    add custom ksovo country

--- lib/country_select/custom_countries.rb
+++ lib/country_select/custom_countries.rb
@@ -1,8 +1,12 @@
 module CountrySelect
   module CustomCountries
     ISO3166::Data.register(
-      alpha2: "XK",
-      name: 'Kosovo'
+      alpha2: 'XK',
+      alpha3: 'XKX',
+      name: 'Kosovo',
+      translations: {
+        'en' => 'Kosovo'
+      }
     )
   end
 end
